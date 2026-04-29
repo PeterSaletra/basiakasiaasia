c3c58cc2ae03ebce86e1a1eb1415c975029ad10b
patrykmil <patryk.miler@student.pk.edu.pl>
2026-04-29 13:28:09 +0200

    feat(nix): add flake configuration and related files

---
+++ frontend/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "BasiaAsiaKasia";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        myShell = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+          ];
+        };
+      in
+      {
+        devShells.default = myShell;
+      }
+    );
+}
